c14aeae69bda716c0e75427d20cd56bcce5c7ff6
Howl <afhernandezb@gmail.com>
2024-08-19 18:04:57 -0300

    lograda funcion encriptar sin restriccion de minusculas y acentos

--- encriptar.js
+++ encriptar.js
@@ -1,3 +1,23 @@
 function encriptar(){
-    
+    var texto = document.getElementById("campoTexto").value;
+    var separado = texto.split('')
+
+    for (let i = 0; i < separado.length + 1; i++){
+        if (separado[i]=="a"){
+            separado[i] = "ai";
+        }else if (separado[i]=="e"){
+            separado[i] = "enter";
+        }else if (separado[i]=="i"){
+            separado[i] = "imes";
+        }else if (separado[i]=="o"){
+            separado[i] = "ober";
+        }else if (separado[i]=="u"){
+            separado[i] = "ufat"
+        }
+   }
+   var resultado = separado.toString();
+   var sinComas = resultado.split(",");
+   var junto = sinComas.join("");
+    console.log(junto);
+    document.getElementById("elTexto").innerText = junto;
 }
